c75aa91f924c8bd78eb1d41b2b5ee1f7d2254cc3
Edu Duarte <eduarteprog@gmail.com>
2022-02-26 15:01:50 -0300

    ligeros cambios al html y actualizacion DB.

--- reehm/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\yakar\onedrive\proyectos\prog\web-django\reehm\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

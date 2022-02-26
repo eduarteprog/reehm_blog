048ba0bb3ca04837f71cbd4ab65b1fb1d0a0ebf6
Edu Duarte <eduarteprog@gmail.com>
2022-02-26 12:26:37 -0300

    Se agregaron que muestre las direcciones url en formato slug.

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.utils import timezone
-
+from slugify import slugify
 
 class Post(models.Model):
+    slug = models.SlugField(blank=True, null=True)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
@@ -14,6 +15,11 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
